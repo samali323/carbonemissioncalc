d2f128e509c70dfa50db26769fd1c0293e72b29f
samali <samali323@gmail.com>
2025-01-06 13:29:25 -0500

    v3

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ if __name__ == "__main__":
     main()
 
 
-#matches tabs not loading data
-#make competition optional
+
 #add other transport options
 #add economic equivalencies and other factors from before
-#choosing from matches tab should fill out the calculator
-#keep all changes in GitHub
