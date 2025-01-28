f0053b1d62f8f63798a3520c6daa7235b5b1b514
samali <samali323@gmail.com>
2025-01-28 13:10:10 -0500

    nested headers not working in streamlit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import streamlit_nested_layout
+
 from src.config.constants import SOCIAL_CARBON_COSTS
 from src.data.team_data import get_all_teams, get_team_airport, get_airport_coordinates, TEAM_COUNTRIES
 from src.models.emissions import EmissionsCalculator
